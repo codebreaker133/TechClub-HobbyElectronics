8ad3d5ca7b916a7c3a18800a735d08bc66a2ab72
codebreaker133 <98831118+codebreaker133@users.noreply.github.com>
2025-10-30 07:56:39 -0500

    cleaning up and prepping for creating library

--- Galaga Breadboard Game/display_test(blinka).py
+++ Galaga Breadboard Game/display_test(blinka).py
@@ -3,21 +3,20 @@ print("hello world")
 import displayio #type: ignore
 import board #type: ignore
 import busio #type: ignore
-from adafruit_edp.edp import Adafruit_EPD #type: ignore
+from adafruit_epd.edp import Adafruit_EPD #type: ignore
 from adafruit_epd.ssd1683 import Adafruit_SSD1683 #type: ignore
-from adafruit_display_text import label #type: ignore
 import terminalio #type: ignore
 print("all modules successfully imported, assigning pins and connecting to display")
 
 #set pin connection variables/names
-SPI_CLOCK = board.GPIO11() # SPI bus clock signal pin
-SPI_MISO = board.GPIO9() # SPI bus input pin
-SPI_MOSI = board.GPIO10() # SPI bus output pin
-EINK_CHIP_SELECT = board.GPIO8() # eink chip enable pin
-EINK_DATACOMMAND = board.GPIO3() # eink data/control pin
-SRAM_SELECT = board.GPIO00() #ram chip select pin  FIX THIS <===========
-EINK_RST = board.GPIO2() # eink reset pin
-EINK_BUSY = board.GPIO14() # eink busy pin
+SPI_CLOCK = board.GPIO11 # SPI bus clock signal pin
+SPI_MISO = board.GPIO9 # SPI bus input pin
+SPI_MOSI = board.D10 # SPI bus output pin
+EINK_CHIP_SELECT = board.GPIO8 # eink chip enable pin
+EINK_DATACOMMAND = board.GPIO3 # eink data/control pin
+SRAM_SELECT = board.GPIO00 #ram chip select pin  FIX THIS <===========
+EINK_RST = board.GPIO2 # eink reset pin
+EINK_BUSY = board.GPIO14 # eink busy pin
 
 #define constants for display peramiters
 DISPLAY_WIDTH = 300
@@ -25,18 +24,19 @@ DISPLAY_HIGHT = 400
 ROTATION = 0
 HILIGHT_COLOR = 0X000000
 print("all pins assigned and values ready to be entered")
-
 SPI = busio.SPI(SPI_CLOCK, SPI_MOSI, SPI_MISO) #define clock and data pins for SPI bus (used to talk to display)
-
-DISPLAY_CONNECTION = Adafruit_SSD1683(DISPLAY_HIGHT, DISPLAY_WIDTH, SPI, EINK_CHIP_SELECT, EINK_DATACOMMAND, SRAM_SELECT, EINK_RST, EINK_BUSY) #creat connection for display with its extra pins
-
+print("SPI bus created, connecting to display")
+display = Adafruit_SSD1683(DISPLAY_HIGHT, DISPLAY_WIDTH, SPI, cs_pin=EINK_CHIP_SELECT, dc_pin=EINK_DATACOMMAND, sram_pin=SRAM_SELECT, rst=EINK_RST, busy_pin=EINK_BUSY) #creat connection for display with its extra pins
+display.rotation(ROTATION)
 WHITE = Adafruit_EPD.WHITE
 BLACK = Adafruit_EPD.BLACK
 RED = Adafruit_EPD.RED
 group = displayio.Group() #create a group to add displayable elements too
-
+display.power_up()
+display.update()
 text = "hello world"
-
-text_area = label.Label(terminalio.FONT, text=text, color=0xFFFFFF, x=32, y=45, scale=2) #creats formated text
-
-display.root_group = group #display group with elements in it
+x=0
+y=0
+display.text(text, x, y, font_name='font5x8.bin')
+display.update()
+display.power_down()
--- Galaga Breadboard Game/pi_display_test.py
+++ Galaga Breadboard Game/pi_display_test.py
@@ -1,9 +1,10 @@
-
+#import dependencies for pin control
 import board #type: ignore
 import busio #type: ignore
+import digitalio #type: ignore
+#import EInk display drivers
 from adafruit_epd.epd import Adafruit_EPD #type: ignore
 from adafruit_epd.ssd1683 import Adafruit_SSD1683 #type: ignore
-import digitalio
 print("all modules successfully imported, assigning pins and connecting to display")
 
 #set pin connection variables/names
@@ -20,25 +21,31 @@ EINK_BUSY = digitalio.DigitalInOut(board.D17) # eink busy pin
 DISPLAY_WIDTH = 300
 DISPLAY_HIGHT = 400
 ROTATION = 0
-
 print("all pins assigned and values ready to be entered")
-SPI = busio.SPI(SPI_CLOCK, SPI_MOSI, SPI_MISO) #define clock and data pins for SPI bus (used to talk to display)
+#define clock and data pins for SPI bus (used to talk to display)
+SPI = busio.SPI(SPI_CLOCK, SPI_MOSI, SPI_MISO) 
 print("SPI bus created, connecting to display")
-display = Adafruit_SSD1683(DISPLAY_HIGHT, DISPLAY_WIDTH, SPI, cs_pin=EINK_CHIP_SELECT, dc_pin=EINK_DATACOMMAND, sramcs_pin=SRAM_SELECT, rst_pin=EINK_RST, busy_pin=EINK_BUSY) #creat connection for display with its extra pins
+
+#create connection for display with its extra pins
+display = Adafruit_SSD1683(DISPLAY_HIGHT, DISPLAY_WIDTH, SPI, cs_pin=EINK_CHIP_SELECT, dc_pin=EINK_DATACOMMAND, sramcs_pin=SRAM_SELECT, rst_pin=EINK_RST, busy_pin=EINK_BUSY) 
+
+#define display colors and rotation
 display.rotation = ROTATION
 WHITE = Adafruit_EPD.WHITE
 BLACK = Adafruit_EPD.BLACK
 RED = Adafruit_EPD.RED
 
-text = "hello dumb asses"
+text = "hello cruel worl"
 x=50
 y=25
-
+import time as T
 print("blanking display")
 display.fill(WHITE)
+T.sleep(3)
+display.display()
 
+T.sleep(8)
 print("displaying text")
 display.text(text, x, y, color=RED, font_name='font5x8.bin')
-
 display.display()
 
