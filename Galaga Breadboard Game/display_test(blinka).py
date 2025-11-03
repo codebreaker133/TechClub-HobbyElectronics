a111f0d054a4a27e8b0863bb0b9e6d66b979387c
CodeBreaker133 <hawrylakhl@gmail.com>
2025-11-03 12:47:30 -0600

    pruning code for branch merge

--- Galaga Breadboard Game/display_test(blinka).py
+++ Galaga Breadboard Game/display_test(blinka).py
@@ -10,20 +10,19 @@ import terminalio #type: ignore
 print("all modules successfully imported, assigning pins and connecting to display")
 
 #set pin connection variables/names
-SPI_CLOCK = board.GPIO11() # SPI bus clock signal pin
-SPI_MISO = board.GPIO9() # SPI bus input pin
-SPI_MOSI = board.GPIO10() # SPI bus output pin
-EINK_CHIP_SELECT = board.GPIO8() # eink chip enable pin
-EINK_DATACOMMAND = board.GPIO3() # eink data/control pin
-SRAM_SELECT = board.GPIO00() #ram chip select pin  FIX THIS <===========
-EINK_RST = board.GPIO2() # eink reset pin
-EINK_BUSY = board.GPIO14() # eink busy pin
+SPI_CLOCK = board.D11  # SPI bus clock signal pin
+SPI_MISO = board.D9  # SPI bus input pin
+SPI_MOSI = board.D10  # SPI bus output pin
+EINK_CHIP_SELECT = board.D8  # eink chip enable pin
+EINK_DATACOMMAND = board.D3  # eink data/control pin
+SRAM_SELECT = board.D00  #ram chip select pin  FIX THIS <===========
+EINK_RST = board.D2  # eink reset pin
+EINK_BUSY = board.D14  # eink busy pin
 
 #define constants for display peramiters
 DISPLAY_WIDTH = 300
 DISPLAY_HIGHT = 400
 ROTATION = 0
-HILIGHT_COLOR = 0X000000
 print("all pins assigned and values ready to be entered")
 
 SPI = busio.SPI(SPI_CLOCK, SPI_MOSI, SPI_MISO) #define clock and data pins for SPI bus (used to talk to display)
@@ -33,10 +32,8 @@ DISPLAY_CONNECTION = Adafruit_SSD1683(DISPLAY_HIGHT, DISPLAY_WIDTH, SPI, EINK_CH
 WHITE = Adafruit_EPD.WHITE
 BLACK = Adafruit_EPD.BLACK
 RED = Adafruit_EPD.RED
-group = displayio.Group() #create a group to add displayable elements too
 
 text = "hello world"
 
-text_area = label.Label(terminalio.FONT, text=text, color=0xFFFFFF, x=32, y=45, scale=2) #creats formated text
 
-display.root_group = group #display group with elements in it
+display.text(x, y, color, )
